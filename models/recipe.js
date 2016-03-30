de36a68cfb54505b0c3846e5fbd2c16be68343bc
Chad <chadsbacon@gmail.com>
2016-03-30 02:11:07 +0000

    Added a model for storing recipes in the database

---
+++ models/recipe.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Recipe = new Schema({
+  id: Schema.Types.ObjectId,
+  title: String,
+  description: String,
+  theme: String,
+  ingredients: [{
+    ingredient: String,
+    amount: Number,
+    unit: String
+  }],
+  instructions: [{
+    instruction: String
+  }],
+  notes: String
+});
+
+module.exports = mongoose.model('Recipe', Recipe);
