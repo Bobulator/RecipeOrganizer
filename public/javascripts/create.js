d79140161ea0486dc89b10e012d651cd8d1a2c61
Chad <chadsbacon@gmail.com>
2016-03-30 01:03:05 +0000

    Took care of some TODO items. Made a little more progress on the create page

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
   var ingredientIndex = 0;
   // When the add button gets clicked create a new ingredient form by cloning the hidden template
-  $('#addIngredientButton').click(function() {
+  $('.addIngredientButton').click(function() {
       console.log("In add ingredient button");
       ingredientIndex++;
 
       var $template = $('#ingredientTemplate');
     
       // Clone the template
-      var $clone    = $template
-                        .clone()
-                        .removeClass('hide')
-                        .removeAttr('id')
-                        .attr('data-ingredient-index', ingredientIndex)
-                        .insertBefore($template);
+      var $clone = $template
+                     .clone()
+                     .removeClass('hide')
+                     .removeAttr('id')
+                     .attr('data-ingredient-index', ingredientIndex)
+                     .insertBefore($template);
         
       // Update the name attributes with the new index
       $clone
         .find('[name="ingredient"]').attr('name', 'ingredient[' + ingredientIndex + '].ingredient').end()
-        .find('[name="amount"]').attr('name', 'ingredient[' + ingredientIndex + '].ingredient').end()
-        .find('[name="unit"]').attr('name', 'ingredient[' + ingredientIndex + '].ingredient').end()
+        .find('[name="amount"]').attr('name', 'ingredient[' + ingredientIndex + '].amount').end()
+        .find('[name="unit"]').attr('name', 'ingredient[' + ingredientIndex + '].unit').end()
   });
 
   // When the remove button gets clicked remove the corresponding row
-  $('#removeIngredientButton').click(function() {
+  $('.removeIngredientButton').click(function() {
     console.log("In remove ingredient button");
     var $row = $(this).parents('.form-group');
     var index = $row.attr('data-ingredient-index');
