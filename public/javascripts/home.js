22e1fbe6b3f5ecd19740ec23c2a9b8537f2e9da9
Chad <chadsbacon@gmail.com>
2016-04-11 03:16:45 +0000

    Added a modal that pops up when you click on a recipe in the home screen. Populated the modal with recipe information.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -7,7 +7,37 @@ $(document).ready(function() {
     console.log($(this).parent().parent().parent().attr('_id'));
     $.getJSON(url, function(result) {
       console.log(result);
-    });
+
+      // Populate header with title, description, and theme
+      $('#recipeModal').find('h3.modal-title').html(result.title);
+      $('#recipeModal').find('p.modal-description').html(result.description);
+      $('#recipeModal').find('p.modal-theme').html('Theme: ' + result.theme);
+      
+      // List ingredients
+      $('#recipeModal').find('ul.modal-ingredients').empty();
+      for (i = 0; i < result.ingredients.length; i++) {
+        var ingredient = "<li>" + result.ingredients[i].amount + " ";
+        ingredient += result.ingredients[i].unit === "None" ? "" : result.ingredients[i].unit;
+        if (result.ingredients[i].amount > 1 && result.ingredients[i].unit !== "None") {
+          ingredient += "s";
+        }
+        ingredient += " " + result.ingredients[i].ingredient + "</li>";
+        $('#recipeModal').find('ul.modal-ingredients').append(ingredient);
+      }
+
+      // List instructions
+      $('#recipeModal').find('ol.modal-instructions').empty();
+      for (i = 0; i < result.instructions.length; i++) {
+        var instruction = "<li>" + result.instructions[i].instruction + "</li>";
+        $('#recipeModal').find('ol.modal-instructions').append(instruction);
+      }
+      
+      // Show notes
+      $('#recipeModal').find('p.modal-notes').html(result.notes);
+
+      // Reveal Modal
+      $('#recipeModal').modal({show:true});
+    });  
   });
 });
 
